3d5b4cc6c6a8210166b2021fe2d6c854b0c645c7
Cesar <cesarxp2@hotmail.com>
2017-01-08 14:58:28 -0700

    Finished

--- js/mainService.js
+++ js/mainService.js
@@ -3,14 +3,14 @@ angular.module('assessApp').service('mainService', function($http) {
     this.getShop = function() {
         return $http({
             method: 'GET',
-            url: 'http://practiceapi.devmounta.in/products'
+            url: 'https://practiceapi.devmounta.in/products'
         })
     }
 
     this.getShop2 = function(id) {
         return $http({
             method: 'GET',
-            url: 'http://practiceapi.devmounta.in/products/' + id
+            url: 'https://practiceapi.devmounta.in/products/' + id
         })
     }
 })
